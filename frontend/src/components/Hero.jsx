e6dea882c836f691b96472e19dcd0d46399fd53c
Alen Chacko <the.alenchacko@gmail.com>
2025-05-06 20:13:47 +0530

    Dummy products rendered

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,7 +5,6 @@ import { assets } from "../assets/frontend_assets/assets.js";
 const Hero = () => {
   return (
     <div className="flex flex-col sm:flex-row border border-gray-400">
-      
       {/* Hero left side */}
       <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
         <div className="text-[#414141]">
