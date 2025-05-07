88c5d9cfd2105e9f23f73fbe0e530b579620f95a
Alen Chacko <the.alenchacko@gmail.com>
2025-05-07 20:36:44 +0530

    added search function

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { assets } from "../assets/frontend_assets/assets.js";
+import { ShopContext } from "../context/ShopContext.jsx";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+  const {setShowSearch} = useContext(ShopContext)
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
@@ -33,7 +35,7 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center gap-6">
-        <img src={assets.search_icon} className="cursor-pointer w-5" alt="" />
+        <img onClick={()=>{setShowSearch(true)}} src={assets.search_icon} className="cursor-pointer w-5" alt="" />
         <div className="group relative ">
           <img
             src={assets.profile_icon}
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -5,7 +5,7 @@ import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
 
 const Collection = () => {
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -30,11 +30,19 @@ const Collection = () => {
 
   const applyFilters = () => {
     let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter((item) =>
+        item.name.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
     if (category.length > 0) {
       productsCopy = productsCopy.filter((item) =>
         category.includes(item.category)
       );
     }
+
     setFilterProducts(productsCopy);
 
     if (subCategory.length > 0) {
@@ -42,6 +50,7 @@ const Collection = () => {
         subCategory.includes(item.subCategory)
       );
     }
+
     setFilterProducts(productsCopy);
   };
 
@@ -64,7 +73,7 @@ const Collection = () => {
 
   useEffect(() => {
     applyFilters();
-  }, [category, subCategory]);
+  }, [category, subCategory, search, showSearch]);
 
   useEffect(() => {
     sortProducts();
