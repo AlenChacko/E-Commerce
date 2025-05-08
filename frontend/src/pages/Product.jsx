2a3ec8a0c42d99261791a808524fa5817b23c070
Alen Chacko <the.alenchacko@gmail.com>
2025-05-08 14:06:19 +0530

    related products rendered in the product page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/frontend_assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
@@ -82,9 +83,42 @@ const Product = () => {
               ))}
             </div>
           </div>
-          {/* Start from here*/}
+
+          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 ">
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
         </div>
       </div>
+      {/* Description & review section */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm ">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+            ratione hic, impedit vel repellat beatae a accusamus amet sunt nulla
+            odio sint aliquid nobis commodi nam cum, libero at omnis.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae,
+            officia placeat fuga cupiditate est ut reiciendis, sit nobis ipsum
+            dolores aliquam alias repellendus explicabo tenetur maxime dolorum
+            repellat dignissimos delectus!
+          </p>
+        </div>
+      </div>
+
+      {/* Display related products */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
+
     </div>
   ) : (
     <div className="opacity-0"></div>
