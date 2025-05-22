f8387b9962026536988d35f04d8dcf549fa40ced
Alen Chacko <the.alenchacko@gmail.com>
2025-05-22 20:55:32 +0530

    added original products

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -9,11 +9,9 @@ const List = ({ token }) => {
   const fetchList = async () => {
     try {
       const response = await axios.get(backendUrl + "/api/product/list");
-      console.log(response.data);
 
       if (response.data.success) {
         setList(response.data.products);
-        // console.log(first)
       } else {
         toast.error(response.data.message);
       }
