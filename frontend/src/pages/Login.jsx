cba1f7d4c22f4434f429cf26b33af2debc9d412d
Alen Chacko <the.alenchacko@gmail.com>
2025-05-22 21:55:57 +0530

    user registration,login,logout added i

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,63 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const [currentState, setCurrentState] = useState("Login");
+  const { token, setToken, navigate, backendUrl } = useContext(ShopContext);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const onSubmitHandler = async (event) => {
     event.preventDefault();
+    try {
+      if (currentState === "Sign up") {
+        if (password !== confirmPassword) {
+          toast.error("Passwords do not match!");
+          return;
+        }
+        const response = await axios.post(backendUrl + "/api/user/register", {
+          firstName,
+          lastName,
+          email,
+          password,
+        });
+        if (response.data.success) {
+          setToken(response.data.token);
+          localStorage.setItem("token", response.data.token);
+          toast.success(response.data.message);
+        } else {
+          toast.error(response.data.message || "Registration failed");
+        }
+      } else {
+        const response = await axios.post(backendUrl + "/api/user/login", {
+          email,
+          password,
+        });
+        console.log(response.data);
+        if (response.data.success) {
+          setToken(response.data.token);
+          localStorage.setItem("token", response.data.token);
+          toast.success(response.data.message);
+        } else {
+          toast.error(response.data.message || "Invalid Email or Password");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response?.data?.message || error.message);
+    }
   };
+
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [token]);
 
   return (
     <form
@@ -25,12 +78,20 @@ const Login = () => {
               placeholder="First Name"
               required
               className="w-1/2 px-3 py-2 border border-gray-800"
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+              value={firstName}
             />
             <input
               type="text"
               placeholder="Last Name"
               required
               className="w-1/2 px-3 py-2 border border-gray-800"
+              onChange={(e) => {
+                setLastName(e.target.value);
+              }}
+              value={lastName}
             />
           </div>
 
@@ -39,18 +100,28 @@ const Login = () => {
             placeholder="Email"
             required
             className="w-full px-3 py-2 border border-gray-800"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
           />
           <input
             type="password"
             placeholder="Password"
             required
             className="w-full px-3 py-2 border border-gray-800"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            value={password}
           />
           <input
             type="password"
             placeholder="Confirm Password"
             required
             className="w-full px-3 py-2 border border-gray-800"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            value={confirmPassword}
           />
         </>
       ) : (
@@ -60,12 +131,20 @@ const Login = () => {
             placeholder="Email"
             required
             className="w-full px-3 py-2 border border-gray-800"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
           />
           <input
             type="password"
             placeholder="Password"
             required
             className="w-full px-3 py-2 border border-gray-800"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            value={password}
           />
         </>
       )}
@@ -77,6 +156,7 @@ const Login = () => {
             className="cursor-pointer"
             onClick={() => {
               setCurrentState("Sign up");
+              setConfirmPassword("");
             }}
           >
             Create account!
@@ -86,6 +166,7 @@ const Login = () => {
             className="cursor-pointer"
             onClick={() => {
               setCurrentState("Login");
+              setConfirmPassword("");
             }}
           >
             Login here
